9f0af76d1e076e3d3cf658261d104a98725b8bf1
antonnell <antonnell.crpto@gmail.com>
2021-03-06 21:36:28 +0200

    Trying
    Signed-off-by: antonnell <antonnell.crpto@gmail.com>

--- pages/swap/[...params].js
+++ pages/swap/[...params].js
@@ -1,3 +1,23 @@
-import Swap from './index.js'
+import Head from 'next/head'
+import Layout from '../../components/layout/layout.js'
+import {
+  Typography,
+  Paper,
+} from '@material-ui/core'
+import classes from './swap.module.css'
+import SwapComponent from '../../components/swapComponent'
+
+function Swap({ changeTheme }) {
+  return (
+    <Layout changeTheme={ changeTheme }>
+      <Head>
+        <title>Swaps</title>
+      </Head>
+      <div className={ classes.swapContainer }>
+        <SwapComponent />
+      </div>
+    </Layout>
+  )
+}
 
 export default Swap
